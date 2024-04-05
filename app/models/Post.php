b56abd659ef60ff3a517e447305045cbdfc3295a
bipasana1 <bipasana12@gmail.com>
2024-04-05 15:29:39 -0400

    new update

--- app/models/Post.php
+++ app/models/Post.php
@@ -2,7 +2,32 @@
 
 namespace app\models;
 
+use app\core\Database;
+
 class Post
 {
     //todo make methods here
+    use Database;
+
+    public function getAllPosts($posts){
+        $query = "select * from posts";
+        return $this->fetchAll($query); 
+    }
+    public function getPostsById($id){
+        $query = "select * from posts where :id";
+        return $this->queryWithParams($query, ['id' => $id]);
+    }
+    public function savePost($inputData){
+        $query = "insert into posts (title, description) values (:title, :description);";
+        return $this->queryWithParams($query, $inputData); 
+    }
+    public function updatePost($inputData) {
+        $query = "update posts set title = :title, description= :description where id = :id"; 
+        return $this->queryWithParams($query, $inputData); 
+    }
+
+    public function deletePost($inputData) {
+        $query = "DELETE FROM posts where id = :id"; 
+        return $this->queryWithParams($query, $inputData); 
+    }
 }
--- sql/question-3-answers.sql
+++ sql/question-3-answers.sql
@@ -4,19 +4,19 @@
 
 -- select all posts where title = includes "title 2":
 
-    Select * From posts Where title = "title 2";
+    SELECT * FROM posts WHERE title LIKE '%title 2%';
 
 -- select all posts and order by the title column alphabetically:
 
-    Select title 
+    Select * 
     from posts 
     Order by title ASC;
 
 -- insert 3 new posts
 
     Insert into posts (title, description) values 
-    ('New Post 1', 'New post description 1');
-    ('New Post 2', 'New post description 2');
+    ('New Post 1', 'New post description 1'),
+    ('New Post 2', 'New post description 2'),
     ('New Post 3', 'New post description 3');
 
 -- update posts where id = 1, set the title to "new title"
