929ae9f8a0d60981e86412659ed93610ebd730d9
bipasana1 <bipasana12@gmail.com>
2024-04-07 09:21:46 -0400

    new

--- app/controllers/PostController.php
+++ app/controllers/PostController.php
@@ -45,7 +45,7 @@ class PostController
         header("Content-Type: application/json");
         if ($id) {
             //TODO 5-c i: get a post data by id
-            $posts = $postModel->getPostsById($id);
+            $posts = $postModel->getPostById($id);
         } else {
             //TODO 5-a: get all posts
             $posts = $postModel->getAllPosts();
